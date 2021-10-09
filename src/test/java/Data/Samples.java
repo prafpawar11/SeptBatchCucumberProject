f3876add10a6b6b304002dbdd684a6baf165dab3
prafpawar11@gmail.com <prafpawar11@gmail.com>
2021-10-09 20:46:45 +0530

    new data

---
+++ src/test/java/Data/Samples.java
@@ -0,0 +1,25 @@
+package Data;
+
+public class Samples {
+
+	public static void main(String[] args) {
+
+		System.out.println("Pune");
+		
+		System.out.println("Pune");
+		
+		
+		System.out.println("Pune");
+		
+		System.out.println("Pune");
+		
+		System.out.println("Pune");
+		
+		System.out.println("Pune");
+		
+		System.out.println("Pune");
+		
+		System.out.println("Pune");
+		System.out.println("Pune");
+	}
+}
