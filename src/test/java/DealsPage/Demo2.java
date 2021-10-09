c03f372377651c220c0e47e1fab7c65775be6827
prafpawar11 <74486812+prafpawar11@users.noreply.github.com>
2021-10-09 20:35:00 +0530

    Update Demo2.java

--- src/test/java/DealsPage/Demo2.java
+++ src/test/java/DealsPage/Demo2.java
@@ -5,6 +5,22 @@ public class Demo2 {
 	public static void main(String[] args) {
 
 		System.out.println("Demo2 file in deals page");
+		System.out.println("Demo2 file in deals page");
+		
+		System.out.println("Demo2 file in deals page");
+		
+		System.out.println("Demo2 file in deals page");
+		
+		
+		System.out.println("Demo2 file in deals page");
+		
+		System.out.println("Demo2 file in deals page");
+		
+		
+		System.out.println("Demo2 file in deals page");
+		
+		
+		System.out.println("Demo2 file in deals page");
 	}
 
 }
